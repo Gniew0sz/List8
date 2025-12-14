6e772f7492d50eed090ee0f494503b4e8fa5b5ff
miesz <mieszalontko0@o2.pl>
2025-12-14 21:33:27 +0100

    Circle functions

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,37 @@
 class Sphere{
-int diameter
+    private int diameter; // properties should be acces through getters for encapsulation purposes this applies to all of them
+    int getDiameter(){
+        return diameter;
+    }
+    void setDiameter(int diameter){
+        this.diameter = diameter;
+    }
+
+
 }
 class Circle{
 int diameter;
+int getDiameter(){
+    return diameter;
+}
+void setDiameter(int diameter){}
+    int getArea(){
+        return diameter*diameter/2*Math.PI;
+    }
+    int getCircumference(){
+        return diameter*Math.PI;
+    }
 }
 
-class Matrix{
+class Matrix{ //once a matrix is defined it should not change its size
     int width;
     int height;
+    int getWidth(){
+        return width;
+    }
+    int getHeight(){
+        return height;
+    }
 
 }
 class Main {
